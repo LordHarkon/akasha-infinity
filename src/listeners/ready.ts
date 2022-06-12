f61aeacb51f71c0d9e600f25bf111be7cb8c4f18
Hooshu <frez758@gmail.com>
2022-06-12 17:11:16 +0300

    Remade the ready event listener

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,13 +1,10 @@
 import { ListenerOptions, PieceContext, Resolvers } from "@sapphire/framework";
 import { Listener, Store } from "@sapphire/framework";
-import { gray, magenta, blue, yellow, green, magentaBright, white } from "colorette";
-
-const version = process.env.VER;
-const dev = process.env.NODE_ENV !== "production";
+import { Permissions } from "discord.js";
+import { PermissionFlagsBits } from "discord-api-types/v10";
+import { capitalize } from "#lib/utils";
 
 export class UserEvent extends Listener {
-    private readonly style = dev ? yellow : blue;
-
     public constructor(context: PieceContext, options?: ListenerOptions) {
         super(context, {
             ...options,
@@ -16,7 +13,6 @@ export class UserEvent extends Listener {
     }
 
     public async run() {
-        this.printStoreDebugInformation();
         this.printBanner();
 
         const time = new Date().toLocaleDateString("ro-RO", {
@@ -26,33 +22,61 @@ export class UserEvent extends Listener {
         });
 
         // TODO: Remove this in production
-        Resolvers.resolveGuildTextChannel(
-            "478556102385074176",
-            this.container.client.guilds?.cache.get("445572484469751808"),
-        ).value.send(`Gateway ready! - ${time}`);
+        if (process.env.NODE_ENV !== "development")
+            await Resolvers.resolveGuildTextChannel("478556102385074176", this.container.client.guilds?.cache.get("445572484469751808")).value.send(
+                `Gateway ready! - ${time}`,
+            );
     }
 
     private printBanner() {
-        const success = green("+");
-
-        const llc = dev ? magentaBright : white;
-        const blc = dev ? magenta : blue;
-
-        this.container.logger.info(`${blc("[")}${success}${blc("]")} ${blc("Gateway")}`);
-        if (dev) this.container.logger.info(dev ? `${blc("<")}${llc("/")}${blc(">")} ${llc("DEVELOPMENT MODE")}` : "");
-        this.container.logger.info(`Version: ${blc(version)}`);
-    }
-
-    private printStoreDebugInformation() {
-        const { client, logger } = this.container;
+        const { colors, logger, client } = this.container;
         const stores = [...client.stores.values()];
-        const last = stores.pop()!;
 
-        for (const store of stores) logger.info(this.styleStore(store, false));
-        logger.info(this.styleStore(last, true));
+        const logoAscii = [
+            "#### ##    ## ######## #### ##    ## #### ######## ##    ##",
+            " ##  ###   ## ##        ##  ###   ##  ##     ##     ##  ## ",
+            " ##  ####  ## ##        ##  ####  ##  ##     ##      ####  ",
+            " ##  ## ## ## ######    ##  ## ## ##  ##     ##       ##   ",
+            " ##  ##  #### ##        ##  ##  ####  ##     ##       ##   ",
+            " ##  ##   ### ##        ##  ##   ###  ##     ##       ##   ",
+            "#### ##    ## ##       #### ##    ## ####    ##       ##   ",
+        ].map((item) => colors.bold(colors.red(item)));
+
+        /**
+         * TODO: Change the 'Origin' part depending on the version.
+         * *Origin is version 1.
+         */
+        const versionString = `${colors.redBright("Version: ")}${colors.green(process.env.VER || "Unknown")} ${colors.red("-")} ${colors.blueBright(
+            "Infinity Origin",
+        )}`;
+        const botTag = `${colors.redBright("Logged in as: ")}${colors.cyanBright(client.user?.tag)} (${colors.green(client.user?.id)})`;
+        const guildCount = `${colors.redBright("Guild count: ")}${colors.cyanBright(client.guilds.cache.size.toLocaleString() || "0")}`;
+        const shardCount = `${colors.redBright("Shard count: ")}${colors.cyanBright(client.shard?.count?.toLocaleString() || "1")}`;
+        const memberCount = `${colors.redBright("Member count: ")}${colors.cyanBright(
+            client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0).toLocaleString(),
+        )}`;
+        const botInvite = `${colors.redBright("Invite application: ")}${colors.cyanBright(
+            client.generateInvite({
+                scopes: ["bot"],
+                // scopes: ['bot', 'applications.commands']
+                permissions: new Permissions([PermissionFlagsBits.Administrator]),
+            }),
+        )}`;
+        const defaultPrefix = `${colors.redBright("Default Prefix: ")}${colors.cyanBright(process.env.PREFIX || "inf.")}`;
+
+        const startMessages = [...logoAscii, "", versionString, "", botTag, guildCount, shardCount, memberCount, botInvite, defaultPrefix];
+
+        for (const store of stores) startMessages.push(this.styleStore(store));
+
+        for (const startMessage of startMessages) {
+            if (startMessage.includes("Invite application") && process.env.NODE_ENV === "development") continue;
+            logger.info(startMessage);
+        }
     }
 
-    private styleStore(store: Store<any>, last: boolean) {
-        return gray(`${last ? "└─" : "├─"} Loaded ${this.style(store.size.toString().padEnd(3, " "))} ${store.name}.`);
+    private styleStore(store: Store<any>) {
+        const { colors } = this.container;
+
+        return `${colors.redBright(`${capitalize(store.name)}: `)}${colors.cyanBright(store.size.toLocaleString())} `;
     }
 }
