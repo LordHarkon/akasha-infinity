a88984e884d2ac9e8cc0e2c4aaee6c87e6fd3500
Hooshu <frez758@gmail.com>
2022-06-12 17:10:59 +0300

    Remade the fetchPrefix method of Client to reflect guild prefixes as well

--- src/lib/structures/Client.ts
+++ src/lib/structures/Client.ts
@@ -1,7 +1,10 @@
-import { SapphireClient, container, SapphireClientOptions } from "@sapphire/framework";
+import { SapphireClient, container, SapphireClientOptions, SapphirePrefix } from "@sapphire/framework";
 import { TaskStore } from "#structures/TaskStore";
 import connectDB from "#lib/databaseUtilities";
 import type { ClientOptions } from "discord.js";
+import type { Message } from "discord.js";
+import { isGuildBasedChannel } from "@sapphire/discord.js-utilities";
+import Settings from "#database/Settings";
 
 export class Client extends SapphireClient {
     public constructor(options: SapphireClientOptions & ClientOptions) {
@@ -11,7 +14,16 @@ export class Client extends SapphireClient {
         container.client = this;
     }
 
-    public override fetchPrefix = (): string => process.env.PREFIX || "inf.";
+    public override fetchPrefix = async (message: Message): Promise<SapphirePrefix> => {
+        if (isGuildBasedChannel(message.channel)) {
+            const settings = new Settings(message.guild);
+            await settings.init();
+
+            return [settings.prefix, process.env.PREFIX || "inf."] as Readonly<string[]>;
+        }
+
+        return process.env.PREFIX || "inf.";
+    };
 
     public override async login(token?: string) {
         container.logger.info("Connecting to the database...");
