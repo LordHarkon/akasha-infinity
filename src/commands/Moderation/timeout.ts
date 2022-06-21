c63e542722529bc564f39fc8e20370dee7da1a8f
Hooshu <frez758@gmail.com>
2022-06-21 04:15:29 +0300

    Added additional case scenarios to timeout

--- src/commands/Moderation/timeout.ts
+++ src/commands/Moderation/timeout.ts
@@ -3,15 +3,17 @@ import { ApplyOptions, RequiresGuildContext } from "@sapphire/decorators";
 import type { CommandOptions } from "#typings/index";
 import type { GuildMember, Message } from "discord.js";
 import { Args } from "@sapphire/framework";
-import { sendLocalized } from "@sapphire/plugin-i18next";
+import { replyLocalized, resolveKey } from "@sapphire/plugin-i18next";
 import ms from "ms";
 
 @ApplyOptions<CommandOptions>({
     name: "timeout",
-    examples: ["timeout TestUser", "timeout TestUser 1h", "timeout TestUser 1h 10m", "timeout TestUser 1h 10m 10s"],
-    usage: "<user> [duration<0, 27d 23h 59m>]",
+    aliases: ["mute"],
+    examples: ["timeout TestUser", "timeout TestUser 1h", "timeout TestUser 1h10m", "timeout TestUser 1h10m10s Bad language."],
+    usage: "<user> [duration<0, 27d23h59m>]",
     preconditions: [
         "GuildOnly",
+        "ModeratorOnly",
         {
             name: "CustomCooldown",
             context: {
@@ -23,59 +25,91 @@ import ms from "ms";
     requiredUserPermissions: ["MODERATE_MEMBERS"],
 })
 export class TimeoutCommand extends Command {
-    @RequiresGuildContext(async (message: Message) => await sendLocalized(message, "errors:guildOnly"))
+    @RequiresGuildContext(async (message: Message) => await replyLocalized(message, "errors:guildOnly"))
     public async messageRun(message: Message, args: Args): Promise<Message | void> {
         let stringTime: string = "";
         const target: GuildMember | null = await args.pick("member").catch(() => null);
         const time: number | null = await args
-            .rest("string")
-            .then((s) => {
-                const splitTime: string[] = s.split(" ");
+            .pick("string")
+            .then(async (s) => {
+                let splitTime: string[] = s.split(/(\d+[dhms])/i).filter((i) => i);
+                let tempStringTime: string[] = [];
                 let fullTime: number = 0;
-                splitTime.forEach((item) => (fullTime += ms(item)));
-                stringTime = s
-                    .split(" ")
-                    .map((item) => ms(ms(item), { long: true }))
-                    .join(" ");
+
+                if (!splitTime) return null;
+
+                function endsWithOneOf(str: string, arr: string[]): boolean {
+                    return arr.some((i) => str.endsWith(i));
+                }
+
+                for (const time of splitTime) {
+                    if (
+                        (time.endsWith("d") && parseInt(time) > 27) ||
+                        (time.endsWith("h") && parseInt(time) > 23) ||
+                        (time.endsWith("m") && parseInt(time) > 59) ||
+                        (time.endsWith("s") && !time.endsWith("ms") && parseInt(time) > 59) ||
+                        (!endsWithOneOf(time, ["d", "h", "m", "s"]) && !time.endsWith("ms"))
+                    ) {
+                        throw new Error(await resolveKey(message, "errors:invalidTime"));
+                    } else {
+                        fullTime += ms(time);
+                        tempStringTime.push(ms(ms(time), { long: true }));
+                    }
+                }
+
+                stringTime = tempStringTime.join(" ");
                 return fullTime;
             })
             .catch(() => null);
 
         if (!target) {
-            return await sendLocalized(message, "commands/timeout:noTarget");
+            return await replyLocalized(message, "commands/timeout:noTarget");
+        }
+
+        if (!target.moderatable) {
+            return await replyLocalized(message, "commands/timeout:noPermissions");
+        }
+
+        if (target.isCommunicationDisabled() && time) {
+            return await replyLocalized(message, {
+                keys: "commands/timeout:alreadyMuted",
+                formatOptions: {
+                    time: `<t:${Math.ceil(target.communicationDisabledUntilTimestamp / 1000)}:R>`,
+                },
+            });
         }
 
         if ((time && time < 0) || (time && time > ms("28d") - ms("1m"))) {
-            return await sendLocalized(message, "commands/timeout:invalidTime");
+            return await replyLocalized(message, "commands/timeout:invalidTime");
+        }
+
+        if (!target.isCommunicationDisabled() && !time) {
+            return await replyLocalized(message, "commands/timeout:notMuted");
         }
 
         try {
-            let success: Message<boolean> | null = null;
+            let success: Message | null = null;
 
-            target.timeout(time);
+            // @ts-ignore
+            await target.timeout(time);
 
             if (time) {
-                success = await sendLocalized(message, {
+                success = await replyLocalized(message, {
                     keys: "commands/timeout:successTimeout",
                     formatOptions: {
                         time: stringTime,
                     },
                 });
             } else {
-                success = await sendLocalized(message, "commands/timeout:successRemove");
+                success = await replyLocalized(message, "commands/timeout:successRemove");
             }
-
-            setTimeout(async () => {
-                await success.delete();
-                await message.delete();
-            }, 5000);
 
             return success;
         } catch (error) {
             // let errorMessage: string | null = null;
             // if (error.code === 50034) errorMessage = await resolveKey(message, "commands/purge:14dayLimit");
             // else errorMessage = await resolveKey(message, "errors:unknown");
-            const msg = await sendLocalized(message, {
+            const msg = await replyLocalized(message, {
                 keys: "errors:unknown",
             });
 
